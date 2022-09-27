fa7270812c600ed674f1b8e62f207818a2bec6bf
Shane Howe <shanehowe100@hotmail.com>
2022-09-27 20:06:59 +0100

    Updated doc strings resemble tests

--- test_bot.py
+++ test_bot.py
@@ -16,6 +16,7 @@ def test_get_joke():
 def test_get_last_seen():
     """
     Tests that the get_last_seen function returns an integer
+    Tests that the get_last_seen function returns the correct ID
     """
     assert isinstance(get_last_seen(TEST_FILE_NAME), int)
     assert get_last_seen(TEST_FILE_NAME) == TEST_ID
@@ -24,6 +25,7 @@ def test_get_last_seen():
 def test_update_last_seen():
     """
     Tests that the update_last_seen function updates the last seen ID
+    Tests that the update_last_seen function returns an integer
     """
     update_last_seen(TEST_FILE_NAME, TEST_ID)
     assert get_last_seen(TEST_FILE_NAME) == TEST_ID
